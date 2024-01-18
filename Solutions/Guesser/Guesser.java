ab90c559ab5722532b19ef0f13a978892f7170ec
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-01-18 10:30:12 +0100

    added initial code

---
+++ Solutions/Guesser/Guesser.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Guesser{
+	public static void main(String[] args){
+		System.out.println("TESTTESTTEST");
+		new Scanner(System.in).nextLine();
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+}
