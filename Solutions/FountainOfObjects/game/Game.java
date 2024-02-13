8df971cd6588c1776391ace1393295cc4ed89bf0
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-02-13 21:20:53 +0100

    Created Game

--- Solutions/FountainOfObjects/game/Game.java
+++ Solutions/FountainOfObjects/game/Game.java
@@ -1,15 +1,113 @@
 package game;
 
-import java.util.Random;
 import map.*;
+import helpers.console.*;
+
+import java.util.Random;
+import java.util.HashMap;
 
 public class Game{
-	private static final int MAP_SIZE = 8;
+	private enum GameState { PLAYING, WON }
+	
+	private static final int MAP_SIZE = 4;
+	
+	private HashMap<RoomType, ConsoleColor> roomTypeColorMap;
 	
 	private Map map;
 	
 	public Game(){
 		map = new MapBuilder().setSize(MAP_SIZE).build();
-		map.display();
+		roomTypeColorMap = new HashMap<RoomType, ConsoleColor>();
+		
+		initRoomTypeColorMap();
+		
+		runGame();
+	}
+	
+	private void runGame(){
+		ConsoleHelper.clearScreen();
+		
+		while(getGameState() == GameState.PLAYING){
+			displayLocation();
+			displayRoomMessage();
+			
+			Command chosenCommand = ConsoleHelper.getCommandInput("what do you want to do?");
+			executeCommand(chosenCommand);
+			
+			ConsoleHelper.clearScreen();
+		}
+		
+		ConsoleHelper.printlnColor("You win!", ConsoleColor.LIGHT_PURPLE);
+	}
+	
+	private void executeCommand(Command command){
+		switch(command){
+			case MOVE_NORTH:
+				move(Cardinal.NORTH);
+				break;
+			case MOVE_EAST:
+				move(Cardinal.EAST);
+				break;
+			case MOVE_SOUTH:
+				move(Cardinal.SOUTH);
+				break;
+			case MOVE_WEST:
+				move(Cardinal.WEST);
+				break;
+			case ENABLE_FOUNTAIN:
+				map.enableFountain();
+				break;
+			case HELP:
+				showHelp();
+				break;
+		}
+	}
+	
+	private void initRoomTypeColorMap(){
+		roomTypeColorMap.put(RoomType.NORMAL, ConsoleColor.WHITE);
+		roomTypeColorMap.put(RoomType.ENTRANCE, ConsoleColor.YELLOW);
+		roomTypeColorMap.put(RoomType.FOUNTAIN, ConsoleColor.BLUE);
+	}
+	
+	private void displayLocation(){
+		RoomLocation location = map.getCurrentRoom().getLocation();
+		System.out.println("You are in the room at location " + location);
+	}
+	
+	private void displayRoomMessage(){
+		String roomMessage =  switch(map.getCurrentRoom().getType()){
+			case RoomType.ENTRANCE -> "You see light in this room coming from outside the cavern. This is the entrance.";
+			case RoomType.FOUNTAIN -> {
+				yield (map.isFountainEnabled()
+					? "You hear the rushing waters from the Fountain of Objects. It has been reactivated!"
+					: "You hear water dripping in this room. The Fountain of Objects is here!"
+				);
+			}
+			default -> null;
+		};
+		
+		if(roomMessage != null){
+			ConsoleHelper.printlnColor(roomMessage, roomTypeColorMap.get(map.getCurrentRoom().getType()));
+		}
+	}
+	
+	private void showHelp(){
+		for(Command command : Command.values()){
+			ConsoleHelper.printlnColor("'" + command.getCommandText() + "': " + command.getDescription(), ConsoleColor.GREEN);
+		}
+	}
+	
+	private void move(Cardinal direction){
+		if(map.getCurrentRoom().hasAdjacentRoom(direction)){
+			map.move(direction);
+		}
+	}
+	
+	private GameState getGameState(){
+		if(map.isFountainEnabled() && map.getCurrentRoom().getType() == RoomType.ENTRANCE){
+			return GameState.WON;
+		}
+		
+		return GameState.PLAYING;
 	}
 }
