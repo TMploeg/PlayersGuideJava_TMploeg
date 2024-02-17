e2d3917f15c30fe8aa71c1dd17aaff943fd25dbf
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-02-17 17:45:15 +0100

    updated map display

--- Solutions/FountainOfObjects/map/Map.java
+++ Solutions/FountainOfObjects/map/Map.java
@@ -6,7 +6,7 @@ import java.awt.Point;
 import java.util.NoSuchElementException;
 
 public class Map {
-  private static final String POS_MARKER = "(x)";
+  private static final String POS_MARKER = "x";
 
   private final Room entrance;
   private Room currentRoom;
@@ -67,86 +67,149 @@ public class Map {
   }
 
   public void display() {
-    Room start = entrance;
-
-    int largestRoomTypeNameLength = 0;
-    for (RoomType roomType : RoomType.values()) {
-      int length = roomType.toString().length();
-      if (length > largestRoomTypeNameLength) {
-        largestRoomTypeNameLength = length;
-      }
-    }
+    Room rowStart = entrance;
 
     while (true) {
-      Room current = start;
-
-      int length = 0;
-
-      int count = 0;
-
-      while (true) {
-        ConsoleColor color =
-            switch (current.getType()) {
-              case NORMAL -> ConsoleColor.WHITE;
-              case ENTRANCE -> ConsoleColor.YELLOW;
-              case FOUNTAIN -> {
-                yield fountainEnabled ? ConsoleColor.BLUE : ConsoleColor.LIGHT_GRAY;
-              }
-              case PIT -> ConsoleColor.DARK_RED;
-              default -> throw new RuntimeException();
-            };
-
-        String roomTypeName = current.getType().toString();
-        ConsoleHelper.printColor(roomTypeName, color);
-
-        boolean isCurrentRoom = current == getCurrentRoom();
-        boolean hasMaelstrom = current.getEntityIfAny(Maelstrom.class) != null;
-
-        int nrOfSpaces =
-            largestRoomTypeNameLength
-                - roomTypeName.length()
-                + ((isCurrentRoom || hasMaelstrom) ? 0 : (POS_MARKER.length() + 1));
-
-        if (isCurrentRoom || hasMaelstrom) {
-          ConsoleHelper.printColor(
-              " " + POS_MARKER, isCurrentRoom ? ConsoleColor.PINK : ConsoleColor.TEAL);
-        }
-
-        for (int i = 0; i < nrOfSpaces; i++) {
-          System.out.print(" ");
-        }
-
-        count++;
-
-        current = current.getAdjacentRoom(Cardinal.EAST);
-
-        if (current == null) {
-          break;
-        }
-
-        System.out.print(" | ");
-      }
+	  displayRow(rowStart);
 
       System.out.println();
 
-      start = start.getAdjacentRoom(Cardinal.SOUTH);
+      rowStart = rowStart.getAdjacentRoom(Cardinal.SOUTH);
 
-      if (start == null) {
+      if (rowStart == null) {
         break;
       }
 
-      String seperator = "";
-
-      for (int i = 0;
-          i
-              < (count * largestRoomTypeNameLength
-                  + (count - 1) * 3
-                  + count * (POS_MARKER.length() + 1));
-          i++) {
-        seperator += "-";
-      }
-
-      System.out.println(seperator);
+	  int nameLength = 1;
+	  int space = 1;
+	  int markerLength = 3;
+	  int leftRightMargin = 1;
+	  int verticalSeperatorWidth = 1;
+	  
+	  int roomDisplayLength = leftRightMargin + nameLength + space + markerLength + leftRightMargin;
+	  
+	  int rowLength = getRowLength(rowStart);
+	  
+	  int horizontalSeperatorLength = roomDisplayLength * rowLength + verticalSeperatorWidth * (rowLength - 1);
+	  
+	  StringBuilder seperatorBuilder = new StringBuilder();
+	  
+	  for(int i = 0; i < horizontalSeperatorLength; i++){
+		seperatorBuilder.append("-");
+	  }
+	  
+      System.out.println(seperatorBuilder.toString());
     }
+  }
+  
+  private void displayRow(Room start){
+	Room current = start;
+	
+	while (true) {
+	  displayRoom(current);
+	  
+	  current = current.getAdjacentRoom(Cardinal.EAST);
+	  
+	  if (current == null) {
+		break;
+	  }
+
+	  ConsoleHelper.printColor("|", ConsoleColor.WHITE);
+	}
+  }
+  
+  private void displayRoom(Room room){
+	System.out.print(" ");
+	
+	printRoomName(room);
+	printRoomEndString(room);
+	
+	System.out.print(" ");
+  }
+  
+  private void printRoomName(Room room){
+	ConsoleColor roomColor = getRoomColor(room);
+	String roomName = getRoomName(room);
+	
+	ConsoleHelper.printColor(roomName, roomColor);
+  }
+  
+  private void printRoomEndString(Room room){
+	System.out.print(" ");
+	
+	boolean isCurrentRoom = room == getCurrentRoom();
+	boolean hasMaelstrom = room.getEntityIfAny(Maelstrom.class) != null;
+	boolean hasAmarok = room.getEntityIfAny(Amarok.class) != null;
+
+	int markerLength = POS_MARKER.length();
+	
+	final int maxNrOfMarkers = 3;
+	int nrOfMarkers = 0;
+	
+	if(isCurrentRoom){
+	  ConsoleHelper.printColor(POS_MARKER, ConsoleColor.PINK);
+	  nrOfMarkers++;
+	}
+	
+	if(hasMaelstrom){
+	  ConsoleHelper.printColor(POS_MARKER, ConsoleColor.TEAL);
+	  nrOfMarkers++;
+	}
+	
+	if(hasAmarok){
+	  ConsoleHelper.printColor(POS_MARKER, ConsoleColor.KUMERA);
+	  nrOfMarkers++;
+	}
+	
+	int nrOfSpaces = (maxNrOfMarkers - nrOfMarkers) * markerLength;
+
+	for (int i = 0; i < nrOfSpaces; i++) {
+	  System.out.print(" ");
+	}
+  }
+  
+  private static int getDefaultRoomLength(){
+	int longestLength = 0;
+	
+    for (RoomType roomType : RoomType.values()) {
+      int length = roomType.toString().length();
+      if (length > longestLength) {
+        longestLength = length;
+      }
+    }
+	
+	return longestLength;
+  }
+  
+  private ConsoleColor getRoomColor(Room room) {
+	return switch (room.getType()) {
+	  case NORMAL -> ConsoleColor.WHITE;
+	  case ENTRANCE -> ConsoleColor.YELLOW;
+	  case FOUNTAIN -> {
+		yield fountainEnabled ? ConsoleColor.BLUE : ConsoleColor.LIGHT_GRAY;
+	  }
+	  case PIT -> ConsoleColor.DARK_RED;
+	  default -> throw new RuntimeException();
+	};
+  }
+  
+  private static String getRoomName(Room room){
+	String name = room.getType().toString();
+	
+	return name.length() > 0 ? name.substring(0,1) : "";
+  }
+  
+  private static int getRowLength(Room rowStart){
+	int length = 0;
+	
+	Room current = rowStart;
+	
+	do{
+	  length++;
+	  current = current.getAdjacentRoom(Cardinal.EAST);
+	}
+	while(current != null);
+	
+	return length;
   }
 }
